f2f2f76f2056ff710186740eaeb9992f1ac31658
nimeimix <huo.linchun@trs.com.cn>
2023-09-21 11:50:24 +0800

    投诉处理路由

---
+++ src/router/routes.js
@@ -0,0 +1,48 @@
+
+import Home from '@/views/index/home-vue.vue';
+
+ const routes = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: Home,
+    mate: {
+      isShow: true,
+      title: '首页'
+    }
+  },
+  {
+    path: '/demo',
+    name: 'demo',
+    redirect: '/demo/index',
+    children: [
+      {
+        path: 'index',
+        name: 'demo-index',
+        component: () => import('@/views/demo/demo-vue.vue'),
+      },
+    ]
+  },
+  // 投诉处理   
+  {
+    path: '/complaintHandling',
+    name: 'complaintHandling',
+    redirect: '/complaintHandling/list',
+    children: [
+      {
+        path: 'list',
+        name: 'complaintHandling-list',
+        component: () => import('@/views/complaint-handling/complaint-handling-list.vue'),
+      },
+    ]
+  },
+  {
+    path: '/:path(.*)',
+    component: () => import('@/views/not-found/not-found.vue')
+  }
+];
+export default routes;
