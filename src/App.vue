3b81759fb88fedeb1e72b755f04082412206ca00
nimeimix <huo.linchun@trs.com.cn>
2023-09-21 15:41:30 +0800

    列表首页

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: nimeimix huo.linchun@trs.com.cn
+ * @Date: 2023-09-21 11:37:50
+ * @LastEditors: nimeimix huo.linchun@trs.com.cn
+ * @LastEditTime: 2023-09-21 14:08:02
+ * @FilePath: /protection-treatment/src/App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <TrsHeader></TrsHeader>
   <div class="web-body">
@@ -24,6 +32,7 @@ import TrsHeader from '@/components/trs-header.vue';
   .web-body {
     width: 100%;
     flex: 1;
+    padding:16px 80px 24px 80px;
   }
 }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
+
 import { defineConfig } from 'vite';
 import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import eslintPlugin from 'vite-plugin-eslint';
 import { viteMockServe } from 'vite-plugin-mock';
-
+// import AutoImport from 'unplugin-auto-import/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    // AutoImport({
+    //   imports:['vue','vue-router']
+    // }),
     eslintPlugin({
 			include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue'],
 		}),
@@ -15,7 +19,8 @@ export default defineConfig({
       supportTs:false,
       logger: true,
       mockPath: './src/mock'
-    })
+    }),
+
   ],
   resolve: {
     alias: {
