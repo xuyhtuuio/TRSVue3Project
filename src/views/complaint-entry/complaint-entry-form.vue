facf8c07146769c279115de4cbba5141c440c33f
cuiyunhao <cuiyunhao@123.com>
2023-09-21 15:22:45 +0800

    feat: update

--- src/views/complaint-entry/complaint-entry-form.vue
+++ src/views/complaint-entry/complaint-entry-form.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>
-    21321312
+  <div class="outter">
+  312312
   </div>
 </template>
 
 <script>
 
+
 </script>
+
+<style>
+.outter {
+  padding: 20px;
+  background-color: white;
+  width: 90%;
+  margin: auto;
+  margin-top: 10px;
+}
+
+
+</style>
