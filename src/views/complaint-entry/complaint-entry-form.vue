f0fda15ffb67bf6998aefb9752f5314fe4e44ef2
cuiyunhao <cuiyunhao@123.com>
2023-09-22 10:55:53 +0800

    feat: 响应式修改

--- src/views/complaint-entry/complaint-entry-form.vue
+++ src/views/complaint-entry/complaint-entry-form.vue
@@ -10,8 +10,8 @@
     <div class="title">
       客户基本信息
     </div>
-    <el-form :inline="true" :model="listQuery" size="small" >
-          <el-form-item label="客户姓名：">
+    <el-form :inline="true" :model="listQuery" size="small" :rules="rules">
+          <el-form-item label="客户姓名：" prop="name">
             <div class="choose-item-background">
                <el-input  placeholder="请输入客户姓名" v-model="listQuery.name"></el-input>
             </div>
@@ -26,18 +26,15 @@
             </div>
             </div>
           </el-form-item>
-          <el-form-item label="商品货号：">
+          <el-form-item label="联系方式：" >
           <div class="choose-item-background">
-            <el-input   placeholder="客户联系方式"></el-input>
+            <el-input  placeholder="客户联系方式"></el-input>
           </div>
 
           </el-form-item>
           <el-form-item label="商品分类：">
             <div class="choose-item-background">
-            <el-cascader
-              clearable
-              :options="textSelection">
-            </el-cascader>
+              <el-input  placeholder="客户联系方式"></el-input>
           </div>
           </el-form-item>
     </el-form>
@@ -58,6 +55,61 @@ import { ref, reactive } from 'vue'
 const radio1 = ref('1')
 const listQuery = reactive({
     name: ''
+})
+
+const rules = ({
+  name: [
+    { required: true, message: 'Please input Activity name', trigger: 'blur' },
+    { min: 3, max: 5, message: 'Length should be 3 to 5', trigger: 'blur' },
+  ],
+  region: [
+    {
+      required: true,
+      message: 'Please select Activity zone',
+      trigger: 'change',
+    },
+  ],
+  count: [
+    {
+      required: true,
+      message: 'Please select Activity count',
+      trigger: 'change',
+    },
+  ],
+  date1: [
+    {
+      type: 'date',
+      required: true,
+      message: 'Please pick a date',
+      trigger: 'change',
+    },
+  ],
+  date2: [
+    {
+      type: 'date',
+      required: true,
+      message: 'Please pick a time',
+      trigger: 'change',
+    },
+  ],
+  type: [
+    {
+      type: 'array',
+      required: true,
+      message: 'Please select at least one activity type',
+      trigger: 'change',
+    },
+  ],
+  resource: [
+    {
+      required: true,
+      message: 'Please select activity resource',
+      trigger: 'change',
+    },
+  ],
+  desc: [
+    { required: true, message: 'Please input activity form', trigger: 'blur' },
+  ],
 })
 </script>
 
@@ -85,6 +137,14 @@ const listQuery = reactive({
 
 .title {
   margin: 10px 0 10px 0;
+}
+
+.el-input__inner {
+  width: 100%;
+  border: none;
+  outline: none;
+  font-size: 14px;
+  background: #F3F6FB;
 }
 
 </style>
