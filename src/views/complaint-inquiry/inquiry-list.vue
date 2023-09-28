674feea09f3aeebc0ee135325a99cd12198f886c
nimeimix <huo.linchun@trs.com.cn>
2023-09-28 15:00:23 +0800

    feat: 投诉

--- src/views/complaint-inquiry/inquiry-list.vue
+++ src/views/complaint-inquiry/inquiry-list.vue
@@ -2,7 +2,7 @@
  * @Author: nimeimix huo.linchun@trs.com.cn
  * @Date: 2023-09-21 11:42:54
  * @LastEditors: nimeimix huo.linchun@trs.com.cn
- * @LastEditTime: 2023-09-28 14:37:57
+ * @LastEditTime: 2023-09-28 14:44:09
  * @FilePath: /protection-treatment/src/views/complaint-handling/complaint-handling-list.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -78,7 +78,7 @@
             </div>
         </div>
         <div v-if="tableData.length" v-loading="search.loading">
-            <el-table class='trs-table' :data="tableData" style="width: 100%;margin-top: 16px;" @sort-change="sortChange">
+            <el-table class='trs-table' :data="tableData" style="width: 100%;margin-top: 16px;" >
                 <el-table-column fixed type="index" label="序号" width="60" align="center">
                     <template #default="scope">
                         <span>{{ (pageValue.pageNow - 1) * pageValue.pageSize + scope.$index + 1 }}</span>
