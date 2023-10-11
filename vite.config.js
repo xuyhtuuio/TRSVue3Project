eb2e8b0a95431c2f8f498bb463b2fcadd7b0ed0f
yft <1766744991@qq.com>
2023-10-11 16:57:43 +0800

    微前端

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: tag => /^micro-app/.test(tag)
+        }
+      }
+    }),
     // viteMockServe({
     //   supportTs:false,
     //   logger: true,
