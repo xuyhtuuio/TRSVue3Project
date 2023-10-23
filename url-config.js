6a41044e22edf906ef5d205f7827ef837a30a64b
yft <1766744991@qq.com>
2023-10-23 17:38:10 +0800

    调整

--- url-config.js
+++ url-config.js
@@ -2,7 +2,7 @@ const config = {
   preDetatl: ''
 }
 if (import.meta.env.MODE === 'product') {
-  config.preDetatl = 'http://192.168.210.57:31608/admin/manage/formManage';
+  config.preDetatl = 'http://192.168.210.57:31705/manageweb';
 } else {
   config.preDetatl = 'http://localhost:8082/admin/manage/formManage'
 }
